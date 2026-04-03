66d7e0ac2c51f07ed3b157698253389b62d16ea9
Azevedo128 <andre.miguel.azevedo@ipvc.pt>
2026-04-03 19:07:29 +0100

    a

---
+++ Assets/Script/CollectableRotatte.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CollectableRotatte: MonoBehaviour
+{
+    [SerializeField] int rotatespeed = 1;
+    void Update()
+    {
+        transform.Rotate(0, rotatespeed, 0,Space.World);
+    }
+}
