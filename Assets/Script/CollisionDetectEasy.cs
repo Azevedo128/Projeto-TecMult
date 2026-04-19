30cc9835f4b5eb75a1a7e6fd6d240b59ddfa91fc
Azevedo128 <andre.miguel.azevedo@ipvc.pt>
2026-04-19 22:02:10 +0100

    Menu Update

---
+++ Assets/Script/CollisionDetectEasy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class CollisionDetectEasy : MonoBehaviour
+{
+    [SerializeField] GameObject thePlayer;
+    [SerializeField] GameObject PlayerAnim;
+    [SerializeField] AudioSource CollisionFX;
+    [SerializeField] GameObject MainCamera;
+    [SerializeField] GameObject FadeOut;
+    void OnTriggerEnter(Collider other)
+    
+    {
+        if (other.CompareTag("Coin"))
+    {
+        return; // ignora completamente
+    }else
+       StartCoroutine(CollisionEnd());
+    }
+
+    
+
+    IEnumerator CollisionEnd()
+    {
+        CollisionFX.Play();
+        thePlayer.GetComponent<PlayerMovement>().enabled=false;
+        PlayerAnim.GetComponent<Animator>().Play("Stumble Backwards");
+        MainCamera.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(2);
+        FadeOut.SetActive(true);
+        yield return new WaitForSeconds(2);
+        MasterInfo.CoinCount = 0;
+        SceneManager.LoadScene(3);
+    }
+}
+
--- Assets/Script/CollisionDetectMedium.cs
+++ Assets/Script/CollisionDetectMedium.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.SceneManagement;
-public class CollisionDetect : MonoBehaviour
+public class CollisionDetectMedium : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject PlayerAnim;
